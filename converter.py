fa66e3fc383d86c97d78ce2604927bba3cdb0d31
JANANI N <jananinaveenkumar448@gmail.com>
2026-06-17 21:17:06 +0530

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,19 @@
+while True:
+    print("----TEMPERATURE CONVERTER----")
+    print("1.CELSIUS TO FAHRENHEIT ")
+    print("2.FAHRENHEIT TO CELSIUS ")
+    print("3.EXIT")
+    choice=input("ENTER CHOICE(1,2,3) ")
+    if choice=="1":
+        c=float(input("ENTER CELSIUS "))
+        f=(c*9/5)+32
+        print(f"{c}C: {f:.2f}F")
+    elif choice=="2" :
+      f = float(input("Enter Fahrenheit: "))
+      c = (f-32)*5/9
+      print(f"{f}F: {c:.2f}C")
+    elif choice=="3":
+       print("EXITING ")
+       break
+    else:
+       print("INVALID CHOICE")
